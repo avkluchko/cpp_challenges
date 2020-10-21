2c8becd4ede930468d4b03954f6ca27a46016adb
Aleksandr Klyuchko <avkluchko@yandex.ru>
2020-10-21 16:50:11 +0500

    Added init project

---
+++ src/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+vector<int> Multiply(const vector<int>& from) {
+    vector<int> result = {};
+
+    return result;
+}
+
+int main() {
+    std::cout << "Hello, World!" << std::endl;
+
+    return 0;
+}
